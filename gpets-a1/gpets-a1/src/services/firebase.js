54fb4524a86296d152e3d8600544a341a9dd14f2
lcvb1997 <lcvb1997@alu.ufc.br>
2026-01-13 13:50:30 -0300

    configuração firebase intergada, back-end com GET em tempo real finalizado

--- gpets-a1/gpets-a1/src/services/firebase.js
+++ gpets-a1/gpets-a1/src/services/firebase.js
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "SUA_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDRkAM1XXS13EBdJ0ISEoVo7ksmruUwAX4",
+  authDomain: "gpets-94781.firebaseapp.com",
+  databaseURL: "https://gpets-94781-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gpets-94781",
+  storageBucket: "gpets-94781.firebasestorage.app",
+  messagingSenderId: "1028215537834",
+  appId: "1:1028215537834:web:c4241331059d035e8e50d3"
 };
 
 const app = initializeApp(firebaseConfig);
